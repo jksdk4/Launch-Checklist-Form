5defc0a9100f8fa63139793e6bf0268b36d01997
Jasmine <jasspears@gmail.com>
2019-12-11 14:49:16 -0600

    fixed JS file, Launch Form now showing up :D

--- script.js
+++ script.js
@@ -12,20 +12,14 @@
 <img src="${}">
 */
 function validateBeforeSubmit(pilot,copilot,fuel,cargo){
-	if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
+    if (pilot.value === "" || copilot.value === "" || fuel.value === "" || cargo.value === "") {
         alert("All fields are required!");
-    	return true;
+        return true;
     } else if (isNaN(cargo.value) || isNaN(fuel.value) || !isNaN(copilot.value)|| !isNaN(pilot.value)){
-    	alert("Make sure to enter valid information for each field!");
-    	return true;
+        alert("Make sure to enter valid information for each field!");
+        return true;
     }
     return false;
-}
-
-function updateLaunchHeader(launchHead,faulty){
-	launchHead.innerText = "Shuttle not ready for launch";
-	launchHead.style.color = 'red';
-	faulty.style.visibility = "visible";
 }
 
 window.addEventListener("load", function(){
@@ -35,60 +29,66 @@ window.addEventListener("load", function(){
 	    let copilotName = document.querySelector("input[name=copilotName]");
 	    let fuelLevel = document.querySelector("input[name=fuelLevel]");
 	    let cargoMass = document.querySelector("input[name=cargoMass]");
-    	let badInput = validateBeforeSubmit(pilotName,copilotName,fuelLevel,cargoMass);
-    	if (badInput){
-    		e.preventDefault();
-		}
+        let badInput = validateBeforeSubmit(pilotName,copilotName,fuelLevel,cargoMass);
+        if (badInput){
+            e.preventDefault();
+        } else {
+            let fetchURL = fetch("https://handlers.education.launchcode.org/static/planets.json");
+            fetchURL.then(function(response){
+                response.json().then(function(json){
+                    const missionTargetDiv = document.getElementById("missionTarget");
+                    let destinationIndex = Math.floor(Math.random()*json.length);
+                    missionTargetDiv.innerHTML = `
+                        <h2>Mission Destination</h2>
+                            <ol>
+                               <li>Name: ${json[destinationIndex].name}</li>
+                               <li>Diameter: ${json[destinationIndex].diameter}</li>
+                               <li>Star: ${json[destinationIndex].star}</li>
+                               <li>Distance from Earth: ${json[destinationIndex].distance}</li>
+                               <li>Number of Moons: ${json[destinationIndex].moons}</li>
+                            </ol>
+                            <img src="${json[destinationIndex].image}">
+                    `;
+                });
+            });
+        }
 
-        let launchStatusCheck = document.getElementById("launchStatusCheck");
+        function updateLaunchHeader(launchHead,faulty){
+            launchHead.innerText = "Shuttle not ready for launch";
+            launchHead.style.color = "red";
+            faulty.style.visibility = "visible";
+        }
+        function resetLaunchHeader(launchHead,faulty){
+            launchHead.innerText = "Awaiting Information Before Launch";
+            launchHead.style.color = "initial";
+            faulty.style.visibility = "hidden";
+        }
+
     	let fuelOkMessage = document.getElementById("fuelStatus");
     	let cargoOkMessage = document.getElementById("cargoStatus");
     	let launchHeader = document.getElementById("launchStatus");
     	let faultyItems = document.getElementById("faultyItems");
-		launchHeader.style.color = "initial";
-    	faultyItems.style.visibility = "hidden";
-    	
-    	if (fuelLevel.value < 10000){
+        let pilotStatus = document.getElementById("pilotStatus");
+        let copilotStatus = document.getElementById("copilotStatus");
+
+        // resets values and formatting each time form is submitted w/o refreshing page
+        resetLaunchHeader(launchHeader,faultyItems);
+
+        if (fuelLevel.value < 10000){
     		fuelOkMessage.innerText = "There is not enough fuel for the journey.";
     		updateLaunchHeader(launchHeader,faultyItems);
-    	}
+    	} else {
+            fuelOkMessage.innerText = "Fuel level high enough for launch";
+        }
     	if (cargoMass.value > 10000){
     		cargoOkMessage.innerText = "There is too much mass for the shuttle to take off.";
     		updateLaunchHeader(launchHeader,faultyItems);
-    	}
+    	} else {
+            cargoOkMessage.innerText = "Cargo mass low enough for launch";
+        }
     	
-    	launchStatusCheck.innerHTML = `
-    		<h2 id="launchStatus"></h2>
-            <div id="faultyItems">
-                <ol>
-                    <li id="pilotStatus">Pilot ${pilotName.value} Ready</li>
-                    <li id="copilotStatus">Co-pilot ${copilotName.value} Ready</li>
-                    <li id="fuelStatus">${fuelOkMessage}</li>
-                    <li id="cargoStatus">${cargoOkMessage}</li>
-                </ol>
-            </div>
-    	`;
-        
+        pilotStatus.innerText = `Pilot ${pilotName.value} Ready`;
+        copilotStatus.innerText = `Co-pilot ${copilotName.value} Ready`;
         e.preventDefault();
-    	console.log(fuelOkMessage.innerText,cargoOkMessage.innerText,launchHeader.innerText, launchHeader.style.color, faultyItems.style.visibility);
-        
-        let fetchURL = fetch("https://handlers.education.launchcode.org/static/planets.json");
-        fetchURL.then(function(response){
-            response.json().then(function(json){
-                const missionTargetDiv = document.getElementById("missionTarget");
-                let destinationIndex = Math.floor(Math.random()*json.length);
-                missionTargetDiv.innerHTML = `
-                    <h2>Mission Destination</h2>
-                        <ol>
-                           <li>Name: ${json[destinationIndex].name}</li>
-                           <li>Diameter: ${json[destinationIndex].diameter}</li>
-                           <li>Star: ${json[destinationIndex].star}</li>
-                           <li>Distance from Earth: ${json[destinationIndex].distance}</li>
-                           <li>Number of Moons: ${json[destinationIndex].moons}</li>
-                        </ol>
-                        <img src="${json[destinationIndex].image}">
-                `;
-            });
-        });
     });
 });
